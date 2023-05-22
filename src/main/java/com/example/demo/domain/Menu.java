b26f9cd8ab3f429bfcf87fd71b968c553d034416
danashinnn <103996283+danashinnn@users.noreply.github.com>
2023-05-22 21:49:23 +0900

    (음식)메뉴 Entity 클래스 구현

---
+++ src/main/java/com/example/demo/domain/Menu.java
@@ -0,0 +1,10 @@
+package com.example.demo.domain;
+
+public class Menu {
+
+    private int menuNo;         // 메뉴 번호
+    private String menuName;    // 메뉴 이름
+    private String menuType;    // 메뉴 타입 (에피타이저, 메인, 사이드, 음료 등)
+    private int menuPrice;      // 메뉴 가격
+    private String menuDesc;    // 메뉴 설명
+}
